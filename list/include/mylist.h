3b2721d6440f2415702b1b626fff631dcb6549d2
xjxian <xjxian02@gmail.com>
2025-08-20 17:16:33 +0800

    the implement of insert and erase

--- list/include/mylist.h
+++ list/include/mylist.h
@@ -1,65 +1,176 @@
 #ifndef MYLIST_H
 #define MYLIST_H
-#include"myallocator.h"
-#include"list_node.h"
-#include"list_iterator.h"
+#include"myallocate.h"
 namespace mystl
 {
-template<class _Ty, class Alloc = MyAllocator<_ListNode<_Ty>>>
-class List
+template<class _Ty, class _A = MyAllocate<_Ty>>
+class MyList
 {
 public:
-    typedef     _Ty            value_type;
-    typedef     _Ty&           reference;
-    typedef     const _Ty&     const_reference;
-    typedef     _Ty*           pointer;
-    typedef     const _Ty*     const_pointer;
-    typedef     size_t         size_type;
-    typedef     int            difference_type;
+    typedef typename _A::difference_type      difference_type;
+    typedef typename _A::size_type            size_type;
+    typedef typename _A::value_type           value_type;
+    typedef typename _A::pointer              pointer;
+    typedef typename _A::reference            reference;
+    typedef typename _A::const_pointer        const_pointer;
+    typedef typename _A::const_reference      const_reference;
+    
+    //----------------list_node------------------------------
+    struct _Node;
+    typedef    _Node*   _NodePtr;
+    struct _Node
+    {
+        _NodePtr _Next;
+        _NodePtr _Prev;
+        value_type _Value;
+    };
+    //----------------ACC------------------------------------
+    struct _Acc
+    {
+        typedef     _NodePtr&       _Nodepref;
+        typedef     value_type&     _Vref;
+        static _Nodepref _Next(_Nodepref _P)
+        {
+            return (_Nodepref)((*_P)._Next);
+        }
+        static _Nodepref _Prev(_Nodepref _P)
+        {
+            return (_Nodepref)((*_P)._Prev);
+        }
+        static _Vref _Value(_Nodepref _P)
+        {
+            return (_Vref)((*_P)._Value);
+        }
+    };
+    //----------------list_iterator----------------------------
+    class Iterator
+    {
+    public:
+        Iterator(_NodePtr node = nullptr) : _Node(node) {}
+        ~Iterator() {}
+        Iterator(const Iterator& it) : _Node(it._Node) {}
+        Iterator& operator = (const Iterator& it) 
+        {
+            if(this == &it) { return *this; }
+            _Node = it._Node;
+            return *this;
+        }
+        bool operator == (const Iterator& other) const
+        {
+            return _Node == other._Node;
+        }
+        bool operator != (const Iterator& other) const
+        {
+            return _Node != other._Node;
+        }
+        reference operator * ()
+        {
+            return _Acc::_Value(_Node);
+        }
+        Iterator& operator ++ ()
+        {
+            this->_Node = _Acc::_Next(_Node);
+            return *this;
+        }
+        Iterator operator ++ (int)
+        {
+            Iterator it = *this;
+            ++(*this);
+            return it;
+        }
+        Iterator& operator -- ()
+        {
+            this->_Node = _Acc::_Prev(_Node);
+            return *this;
+        }
+        Iterator operator -- (int)
+        {
+            Iterator it = *this;
+            --(*this);
+            return it;
+        }
+        _NodePtr _MyNode() const
+        {
+            return _Node;
+        }
+    protected:
+        _NodePtr _Node;
+    };
+    //--------------------construct/destruct---------------------
+    explicit MyList() : _Head(_BuyNode()), _Size(0) {}
 
-    typedef     Iterator<_Ty>  iterator;
-    typedef     _ListNode<_Ty> node;
-    typedef     node*          _NodePtr;
 
-    List()
+    //-------------------begin()/end()----------------------
+    Iterator begin()
     {
-        _Head = _BuyNode();
-        _Head->_Next = _Head;
-        _Head->_Prev = _Head;
+        return Iterator(_Acc::_Next(_Head));
     }
-    iterator begin()
+    Iterator end() const
     {
-        return iterator(_Head->_Next);
+        return Iterator(_Head);
     }
-    iterator end()
+    //------------------------insert------------------------
+    Iterator insert(Iterator _P, const_reference _V = _Ty())
     {
-        return iterator(_Head);
+        _NodePtr _S = _P._MyNode();
+        _Acc::_Prev(_S) = _BuyNode(_S, _Acc::_Prev(_S));
+        _S = _Acc::_Prev(_S);
+        _Acc::_Next(_Acc::_Prev(_S)) = _S;
+        _Alloc.construct(&_Acc::_Value(_S), _V);
+        _Size++;
+        return Iterator(_S);
     }
-    //insert
-    iterator insert(iterator it, const value_type& val)
+    Iterator insert(Iterator _P, size_type _N ,const_reference _V = _Ty())
     {
-        _NodePtr tmp = _BuyNode();
-        tmp->_Value = val;
-        tmp->_Next = it._Node;
-        tmp->_Prev = it._Node->_Prev;
-        tmp->_Prev->_Next = tmp;
-        it._Node->_Prev = tmp;
-        return iterator(tmp);
+        for(int i = 0; i < _N; ++i)
+        {
+            insert(_P, _V);
+        }
     }
-    //https://www.bilibili.com/video/BV1UTcBe1Egi/?spm_id_from=333.337.search-card.all.click&vd_source=2f952035f10e5522a8ae5d16228272bc
+    Iterator insert(Iterator _P, Iterator _F, Iterator _L)
+    {
+        while (_F != _L)
+        {
+            insert(_P, *_F);
+            ++_F;
+        }
+        
+    }
+    //------------------------erase------------------------
+    Iterator erase(Iterator _P)
+    {
+        _NodePtr ptr = (_P++)._MyNode();
+        _Acc::_Next(_Acc::_Prev(ptr)) = _Acc::_Next(ptr);
+        _Acc::_Prev(_Acc::_Next(ptr)) = _Acc::_Prev(ptr);
+        _Alloc.destroy(&_Acc::_Value(ptr));
+        _FreeNode(ptr);
+        _Size--;
+        return _P;
+    }
+    Iterator erase(Iterator _F, Iterator _L)
+    {
+        while (_F != _L)
+        {
+            erase(_F++);
+        }
+        return _F;
+    }
 private:
-    _NodePtr _BuyNode() const
+    _NodePtr _BuyNode(_NodePtr _Narg = nullptr, _NodePtr _Parg = nullptr)
     {
-        return alloc.allocate(1);
+        _NodePtr _S = (_NodePtr)_Alloc._Charalloc(sizeof(_Node));
+        _Acc::_Next(_S) = (_Narg != nullptr ? _Narg : _S);
+        _Acc::_Prev(_S) = (_Parg != nullptr ? _Parg : _S);
+        return _S;
     }
-    _NodePtr _FreeNode(_NodePtr _P) const
+    void _FreeNode(_NodePtr _P)
     {
-        alloc.desallocate(_P);
+        _Alloc.deallocate(_P);
     }
 private:
     _NodePtr _Head;
-    Alloc alloc;
+    size_type _Size;
+    _A _Alloc;
 };
 }
-
 #endif
--- list/src/main.cpp
+++ list/src/main.cpp
@@ -1,16 +1,32 @@
-#include<iostream>
 #include"mylist.h"
 int main()
 {
-    mystl::List<int> list;
-    for(int i = 1; i <= 5; ++i){
-        list.insert(list.end(), i);
-    }
-    for(int i = 6; i <= 10; ++i){
+    mystl::MyList<int> list;
+    for(int i = 0; i < 5; ++i)
+    {
         list.insert(list.begin(), i);
     }
-    for(auto it = list.begin(); it != list.end(); ++it){
-        std::cout << *(it) << std::endl;
+    for(int i = 0; i < 5; ++i)
+    {
+        list.insert(list.end(), i);
     }
+    for(auto it = list.begin(); it != list.end(); ++it)
+    {
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+    list.erase(list.begin());   
+    list.erase(--list.end());
+    for(auto it = list.begin(); it != list.end(); ++it)
+    {
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+    list.erase(list.begin(), list.end());
+    for(auto it = list.begin(); it != list.end(); ++it)
+    {
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
     return 0;
 }
